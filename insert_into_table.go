46a686ad304795c0c1b052ba2cdafa729af7f41d
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-10-06 19:17:44 +0000

    Changed username to match linode tutorial

--- insert_into_table.go
+++ insert_into_table.go
@@ -11,7 +11,7 @@ func main() {
 
 	// Open up our db connection.
 	// The db is called testdb.
-	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/testdb")
+	db, err := sql.Open("mysql", "testuser:password@tcp(127.0.0.1:3306)/testdb")
 
 	// if there is an error opening the connection, handle it
 	if err != nil {
