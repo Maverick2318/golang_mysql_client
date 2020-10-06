c6d8a087be4ee063035160904e685f529f9718c5
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-10-06 19:12:56 +0000

    Querying a single row in a table and outputting the results into a struct

---
+++ query_single_row.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type Customer struct {
+	ID        int    `json:"customer_id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our db connection.
+	// The db is called testdb.
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/testdb")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close until after main() has finished
+	defer db.Close()
+
+	var customer Customer
+
+	// Execute the query
+	err = db.QueryRow("SELECT customer_id, first_name, last_name FROM customers where customer_id = ?", 1).Scan(&customer.ID, &customer.FirstName, &customer.LastName)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println(customer.FirstName)
+	log.Println(customer.LastName)
+}
