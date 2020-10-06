b855cf9e87d1abd1fd209604b2b239f7f101bf75
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2020-10-06 18:56:12 +0000

    An example of writing query results into a Go struct

---
+++ write_results_to_struct.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type Customer struct {
+	ID	int `json:"customer_id"`
+	FirstName	string	`json:"first_name"`
+	LastName	string	`json:"last_name"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our db connection.
+	// The db is called testdb.
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/testdb")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close until after main() has finished
+	defer db.Close()
+
+        // Execute the query
+        results, err := db.Query("SELECT customer_id, first_name, last_name FROM customers")
+
+	for results.Next() {
+		var customer Customer
+
+		// for each row, scan the result into our customer composite object
+		err = results.Scan(&customer.ID, &customer.FirstName, &customer.LastName)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		log.Printf(customer.FirstName)
+		log.Printf(customer.LastName)
+	}
+}
